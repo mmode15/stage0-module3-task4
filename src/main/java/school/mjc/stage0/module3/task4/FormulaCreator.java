d0b3494cfc956d114e5c566912f2ef648a91222f
Maiko <maiko.mode05@gmail.com>
2023-09-12 01:59:04 +0400

    done

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -1,6 +1,14 @@
 package school.mjc.stage0.module3.task4;
 
+import java.util.function.Function;
+
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        System.out.println(f(2,3));
     }
+    static int f(int a, int b){
+        return (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+    }
+
 }
